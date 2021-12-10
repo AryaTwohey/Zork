fb3b329cd65b0c704f8483a8aaab326d84c67744
Arya Twohey <MY_NAME@example.com>
2021-12-10 12:55:45 -0500

    Inventory Class

--- Zork/src/zork/Inventory.java
+++ Zork/src/zork/Inventory.java
@@ -1,8 +1,8 @@
 package zork;
-
 import java.util.ArrayList;
 
 public class Inventory {
+
   private ArrayList<Item> items;
   private int maxWeight;
   private int currentWeight;
@@ -25,9 +25,44 @@ public class Inventory {
     if (item.getWeight() + currentWeight <= maxWeight)
       return items.add(item);
     else {
-      System.out.println("There is no room to add the item.");
+      System.out.println("There is no room to add the item, please drop something.");
       return false;
     }
   }
 
+  public boolean dropItem(Command command) {
+
+    if(!command.hasSecondWord()){
+
+      System.out.println();
+      System.out.println("Drop What?");
+      
+    }else{
+
+  String itemName = command.getSecondWord();
+      
+    for (int i = 0; i < items.size(); i++) {
+
+      if (itemName.equals(items.get(i).getName())) {
+        items.remove(i);
+        return true;
+      }
+    }
+  }
+  return false;
+
+}
+
+  public String toString(){
+    String msg = "";
+
+    for (Item item : items){
+      msg += item + ", ";
+    }
+
+    if (msg.length()>0){
+      msg = msg.substring(msg.length()-2);
+    }
+    return msg;
+  }
 }
