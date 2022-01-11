2e9b83648dcd46a31161ab7f79f2288faac4ffc9
Lara Sharma <lsharma@bayviewglen.ca>
2022-01-10 21:27:00 -0500

    note

--- Zork/src/zork/Note.java
+++ Zork/src/zork/Note.java
@@ -1,5 +1,66 @@
 package zork;
 
-public class Note{ 
+public class Note extends Item{ 
+  private int weight;
+  private String name;
+  private boolean isOpenable;
+  private String description; 
+
+  
+  public Note(int weight, String name, boolean isOpenable, String description) {
+    this.weight = weight;
+    this.name = name;
+    this.isOpenable = isOpenable;
+    this.description = description; 
+    
+  }
+  
+
+  public Note() {
+  }
+
+  public void open() {
+    if (!isOpenable)
+      System.out.println("The " + name + " cannot be opened.");
+
+  }
+
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+ public String getDescription() {
+    return description;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean isOpenable() {
+    return isOpenable;
+  }
+
+  public void setOpenable(boolean isOpenable) {
+    this.isOpenable = isOpenable;
+  }
+
+
+
 
 }
+
+
